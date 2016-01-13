94684bf5786b9976d639db874abfee6ffbe8dea4
Daniel Meixner <daniel.meixner@live.com>
2016-01-13 15:25:31 +0100

    SO

---
+++ JSAppGeolocation/JSAppGeolocation/js/default.js
@@ -0,0 +1,68 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+				// TODO: This application has been newly launched. Initialize your application here.
+			} else {
+				// TODO: This application was suspended and then terminated.
+				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+			}
+			args.setPromise(WinJS.UI.processAll());
+		}
+
+        getLocationForMap();
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+
+	function getLocationForMap() {
+	    Windows.Devices.Geolocation.Geolocator.requestAccessAsync().done(
+            function (accessStatus) {
+                switch (accessStatus) {
+                    case Windows.Devices.Geolocation.GeolocationAccessStatus.allowed:
+
+                        // call the WinRT Component
+                        var rc = RuntimeComponent1.Class1;
+
+                        // use promises to wait for completion
+                        rc.getGeopoisitionAsync().then(function (res) {
+                                                      
+                            // access the props of the result
+                            var m = new Windows.UI.Popups.MessageDialog(res.coordinate.latitude);
+
+                            // show latitude in message box
+                            m.showAsync();
+
+                        });
+                     
+                        break;
+
+                    case Windows.Devices.Geolocation.GeolocationAccessStatus.denied:
+                        WinJS.log && WinJS.log("Access to location is denied.", "sample", "error");
+                        break;
+
+                    case Windows.Devices.Geolocation.GeolocationAccessStatus.unspecified:
+                        WinJS.log && WinJS.log("Unspecified error!", "sample", "error");
+                        break;
+                }
+            },
+            function (err) {
+                WinJS.log && WinJS.log(err, "sample", "error");
+            });
+	}
+
+
+	app.start();
+})();
---
+++ JSAppGeolocation/RuntimeComponent1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Services.Maps;
+
+namespace RuntimeComponent1
+{
+    public sealed class Class1
+    {
+        public static  IAsyncOperation <Geoposition> GetGeopoisitionAsync()
+        {
+            Geolocator g = new Geolocator();
+            return g.GetGeopositionAsync() as IAsyncOperation<Geoposition> ;                     // get the raw geoposition data                      
+            
+        }
+
+
+        //public static Geoposition GetGeolocation()
+        //{
+        //    Geolocator _geolocator = new Geolocator();
+        //    MapLocation mapInfo = null;
+
+        //    _geolocator.DesiredAccuracyInMeters = 5;
+
+        //    try
+        //    {
+        //        Geoposition currentPosition = GetGeopoisitionWrapper(_geolocator).Result;
+        //        //      Geoposition currentPosition = GetGeopoisitionWrapper(_geolocator).AsAsyncOperation().GetResults(); 
+
+        //        //other stuff needing currentPosition, and another async function call
+
+        //        return currentPosition;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return null;
+        //    }
+
+        //}
+
+    }
+
+   
+}
